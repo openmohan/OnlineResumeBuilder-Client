bedbb334300ab0634a08e888a714305a25d31ede
mohanprasaths <mohanprasath@live.com>
2017-03-24 16:31:02 +0530

    check LinkedINSDK

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 import styles from './Home.css'
 
 var Home = React.createClass({
+  componentWillMount: function() {
+
+  },
   render : function(){
   return (
     <div className={styles.greenColor}>
-      Homeh
+      MOhan
     </div>
   )}
 })
